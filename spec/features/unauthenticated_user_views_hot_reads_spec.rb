cae220e409583ead7ccee6389d671fd0638a6fb2
Calaway <calaway@users.noreply.github.com>
2016-12-19 16:53:47 -0700

    Stub out feature test for reads index.

---
+++ spec/features/unauthenticated_user_views_hot_reads_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.feature "UnauthenticatedUserViewsHotReads", type: :feature do
+  context "one url has been read" do
+    scenario "they see the top read urls" do
+      
+    end
+  end
+end

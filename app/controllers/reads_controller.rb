80666dabdffe2a3710f67e9d9202eeef55098e37
Calaway <calaway@users.noreply.github.com>
2016-12-19 21:53:57 -0700

    Send response from post.

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -1,6 +1,7 @@
 class ReadsController < ApplicationController
   def create
-    Read.create(url: params["read"]["url"])
+    read = Read.create(url: params["read"]["url"])
+    render json: read, status: 201
   end
 
   def index
